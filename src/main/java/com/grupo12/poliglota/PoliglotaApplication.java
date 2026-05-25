fedc02c49174ddb6b71308842aa802477cd775b9
chinoferretti <chinoferretti@gmail.com>
2026-05-24 21:32:13 -0300

    Base con config SpringBoot + Docker + NoSQL

---
+++ src/main/java/com/grupo12/poliglota/PoliglotaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo12.poliglota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliglotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliglotaApplication.class, args);
+	}
+
+}
